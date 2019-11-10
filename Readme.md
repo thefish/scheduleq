b31a5688b6c44471aab36335892334949eebdbfd
thefish <thefish@zaar.be>
2019-11-10 03:12:21 +0300

    readme and test

---
+++ Readme.md
@@ -0,0 +1,33 @@
+Timed queue
+---
+
+Done by some article, i've made some improvement
+
+the basic idea is to plan execution of aritrary closures in future.
+
+```go
+import(
+	"github.com/thefish/scheduleq"
+	"time"
+)
+
+func main() {
+	q := scheduleq.Newqueue()
+	q.Plan(func(){
+		fmt.Println("trololo")
+	}, 1400)
+	
+	//then some time passes
+	time.Sleep(time.Second)
+	//but nothing happens
+	q.Advance()
+	//then some mmore time passes
+	time.Sleep(time.Second)
+	//and we get our message printed! 
+	q.Advance()
+}
+
+```
+
+Useful to process events in time quants, allowing us to fall behind to some degree if payload in functions took some 
+more time than was planned. There are some gotchas with scopes, see test for details and take note. 
--- scheduleq_test.go
+++ scheduleq_test.go
@@ -1,24 +1,37 @@
 package scheduleq
 
 import (
-	"fmt"
+	"github.com/thefish/scheduleq"
 	"testing"
 	"time"
 )
 
-func TestScheduleQueue(t *testing.T) {
-	var test = 0
-	q := NewQueue()
-	q.Plan(func(){
-		test = test + 1
-	}, 100)
-	q.Plan(func(){
-		test = test + 2
-	}, 100)
-	q.Plan(func(){
-		test = test + 3
-	}, 100)
+type stateHolder struct {
+	counter int
+}
 
+func TestScheduleQueue(t *testing.T) {
+
+	sh := &stateHolder{counter: 0}
+	cnt := 0
+	q := scheduleq.NewQueue()
+
+	for i := 1; i < 4; i++ {
+		increment := i //because _current_ i would be out of scope, instead LAST value will be used, and you'll get 12 as result
+		cnt = cnt      //or you can do this as a variant, looks sick imo, but nevertheless binds to outer scope
+		q.Plan(func() {
+			func(holder *stateHolder) {
+				cnt = cnt + increment
+				holder.counter = holder.counter + increment
+				t.Log("Planning to increase by ", increment)
+			}(sh)
+		}, 100)
+	}
 	time.Sleep(time.Second)
-	fmt.Printf("test is: %d", test)
+	q.Advance(time.Now())
+	t.Log("holder struct = ", sh.counter)
+	t.Log("local variable = ", cnt)
+	if sh.counter != 6 || cnt != 6 {
+		t.Fail()
+	}
 }
